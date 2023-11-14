a609c41e52929de5db88715682d1bd38d11c467f
Tien Cam Ly <tily@itu.dk>
2023-11-14 19:47:23 +0100

    Setting up client/node

---
+++ Nodes/Node.go
@@ -0,0 +1,8 @@
+package Nodes
+
+type Node struct {
+	//proto.UnimplementedParticipantServiceServer// Necessary
+	id          int
+	portNumber  int
+	lamportTime int
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Tien197/Mandatory-Handin-4-Distributed-Mutual-Exclusion
+
+go 1.21.0
